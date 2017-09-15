dd91949407b99dba96263846860b50c2565a109b
Cfx <Cfx@192.168.0.164>
2017-09-15 15:33:26 +0200

    add missing files

---
+++ Timeplan/src/app/core/AppDateApdater.ts
@@ -0,0 +1,42 @@
+import { NativeDateAdapter } from "@angular/material";
+
+
+export class AppDateAdapter extends NativeDateAdapter {
+
+    format(date: Date, displayFormat: Object): string {
+
+        if (displayFormat === 'input') {
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            return `${day}.${month}.${year}`;
+        } else {
+            return date.toDateString();
+        }
+    
+    }
+  parse(value: any): Date | null {
+    if ((typeof value === 'string') && (value.indexOf('.') > -1)) {
+      const str = value.split('.');
+      if (str.length < 2 || isNaN(+str[0]) || isNaN(+str[1]) || isNaN(+str[2])) {
+        return null;
+      }
+      return new Date(Number(str[2]), Number(str[1]) - 1, Number(str[0]), 12);
+    }
+    const timestamp = typeof value === 'number' ? value : Date.parse(value);
+    return isNaN(timestamp) ? null : new Date(timestamp);
+  }
+}
+
+export const APP_DATE_FORMATS =
+{
+    parse: {
+        dateInput: { month: 'numeric', year: 'numeric', day: 'numeric' },
+    },
+    display: {
+        dateInput: 'input',
+        monthYearLabel: { year: 'numeric', month: 'numeric' },
+        dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+        monthYearA11yLabel: { year: 'numeric', month: 'long' },
+    }
+};
